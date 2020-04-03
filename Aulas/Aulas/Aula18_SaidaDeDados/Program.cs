9e271a7e82bb8ce6ec4b7c47661a7415be9ef608
Felipe <felipe-sartori@outlook.com>
2020-04-03 00:02:37 -0300

    Adicionados arquivos das aulas vistas

---
+++ Aulas/Aulas/Aula18_SaidaDeDados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double saldo = 10.35784;
+            int idade = 32;
+            string nome = "Maria";
+
+            Console.Write("Olá mundo!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Até mais.");
+
+            Console.WriteLine();
+            Console.WriteLine(saldo);
+            Console.WriteLine(saldo.ToString("F2")); // .toString("F-Quantia de casas que quer exibir")
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture)); // Isso vai fazer imprimir com pontos ao invés de vírgula
+
+            //Técnica do PLACEHOLDER
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais",nome, idade, saldo);
+
+            //Técnica da INTERPOLAÇÃO
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            Console.ReadLine();
+        }
+    }
+}
