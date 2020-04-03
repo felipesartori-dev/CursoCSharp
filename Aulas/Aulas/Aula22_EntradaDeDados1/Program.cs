2d1bb6672fbff2395322100b00ad4448c0b63873
Felipe <felipe-sartori@outlook.com>
2020-04-03 15:42:16 -0300

    Adicionado conteudo visto nas aulas

---
+++ Aulas/Aulas/Aula22_EntradaDeDados1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula22_EntradaDeDados1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Utilizando o SPLIT - serve para quebrar uma string em vários conteudos e armazenar em um vetor.
+
+            String recebePalavras = Console.ReadLine();
+
+            Console.WriteLine(recebePalavras);
+
+            /* Agora vamos utilizar o split pra armazenar cada palavra em uma posição do vetor e depois armazena-las
+            em uma variável */
+            // Primeiro é necessária a criação de um vetor
+
+            string[] vetorPalavras = recebePalavras.Split(' '); // O espaço em branco entre aspas simples indica que sempre que o programa encontrar um espaço em branco irá dividir as palavras
+            
+            string p0 = vetorPalavras[0];
+            string p1 = vetorPalavras[1];
+            string p2 = vetorPalavras[2];
+
+            Console.WriteLine($"{p0} \n{p1} \n{p2}");
+        }
+    }
+}
---
+++ Exercicios/Exercicios/Exercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1_aula18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine($"{produto1} cujo preço é R${preco1:F2}");
+            Console.WriteLine($"{produto2} cujo preço é R${preco2:F2}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero {genero}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondando (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal Invariant Culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
